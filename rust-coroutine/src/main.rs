362ffba699efbc53e5492d827268663decd587d2
Kirill Pimenov <kirill@pimenov.cc>
2016-02-21 14:22:44 +0100

    Coroutine+threads

--- rust-coroutine/src/main.rs
+++ rust-coroutine/src/main.rs
@@ -1,6 +1,14 @@
 extern crate coroutine;
 
+use std::thread;
 use coroutine::asymmetric::Coroutine;
+
+fn run_skynet(start: u64, total: u64) -> u64 {
+    let coro = Coroutine::spawn(&skynet);
+    let (x, _) = coro.resume_with((start, total)).unwrap().unwrap();
+    coro.resume().unwrap();
+    return x;
+}
 
 fn skynet(me: coroutine::asymmetric::CoroutineRef<(u64, u64)>) {
     let (my_number, remaining) = me.take_data().unwrap();
@@ -8,11 +16,7 @@ fn skynet(me: coroutine::asymmetric::CoroutineRef<(u64, u64)>) {
     if remaining==1 {
         me.yield_with((my_number, 0));
     } else {
-        let mut coros: Vec<coroutine::asymmetric::Coroutine<(u64, u64)>> = Vec::with_capacity(10);
-
-        for _ in 0..10 {
-            coros.push(Coroutine::spawn(&skynet));
-        }
+        let coros: Vec<_> = (0..10).map(|_| Coroutine::spawn(&skynet)).collect();
 
         let mut res = 0u64;
         for i in 0..10 {
@@ -26,10 +30,10 @@ fn skynet(me: coroutine::asymmetric::CoroutineRef<(u64, u64)>) {
 }
 
 fn main() {
-    let coro: Coroutine<(u64, u64)> = Coroutine::spawn(&skynet);
-    let (num, _) = coro.resume_with((0,1000000)).unwrap().unwrap();
+    let num = (0..10)
+        .map(|i| thread::spawn(move || run_skynet(i*100000, 100000)))
+        .map(|thread| thread.join().unwrap())
+        .fold(0, std::ops::Add::add);
 
     println!("{}", num);
-
-    coro.resume().unwrap();
 }
